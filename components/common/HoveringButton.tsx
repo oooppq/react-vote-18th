dbcd6597c5e5c2160194deeeb1ea24172b57c4eb
DaegyunOh <oooppq@naver.com>
2023-12-19 18:26:22 +0900

    chore: hovering button text weight semibold 설정

--- components/common/HoveringButton.tsx
+++ components/common/HoveringButton.tsx
@@ -13,7 +13,7 @@ const HoveringButton = ({
 }: HoveringButtonProps) => {
   return (
     <button
-      className={`${buttonStyle} text-white rounded-[20px] transition ease-in-out delay-100 bg-ceos-1 hover:-translate-y-1 hover:scale-110 hover:bg-ceos-2 duration-300`}
+      className={`${buttonStyle} font-semibold text-white rounded-[20px] transition ease-in-out delay-100 bg-ceos-1 hover:-translate-y-1 hover:scale-110 hover:bg-ceos-2 duration-300`}
       onClick={handleClickButton}
     >
       {children}
