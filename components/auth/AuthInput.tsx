6c2d7e35943d0d9e16df54a9943bc55ba5f9b2fb
DaegyunOh <oooppq@naver.com>
2023-12-21 16:43:08 +0900

    style: 로그인/회원가입 input 디자인 조금 변경

--- components/auth/AuthInput.tsx
+++ components/auth/AuthInput.tsx
@@ -10,7 +10,7 @@ const AuthInput = ({ placeHolder, inputName, isPassword }: AuthInputProps) => {
   return (
     <input
       type={isPassword ? 'password' : 'text'}
-      className="w-full border border-[#e3e3e3] bg-[#f4f4f4] rounded-[5px] px-3 py-2 text-sm focus:outline-1 focus:outline-[#e1e1e1]"
+      className="w-full border-b border-[#e3e3e3]px-3 px-5 py-2 text-sm"
       placeholder={placeHolder}
       name={inputName}
     />
