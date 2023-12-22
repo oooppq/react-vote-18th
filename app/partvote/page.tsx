5636f542d7d97f41618924fae58a5b3245fd9f17
Jiseok <chae0_@naver.com>
2023-12-22 19:29:57 +0900

    feat: partvote 페이지 초기완성

---
+++ app/partvote/page.tsx
@@ -0,0 +1,71 @@
+'use client';
+import React, { useState } from 'react';
+import VoteItem from '@/components/vote/VoteItem';
+import HoveringButton from '@/components/common/HoveringButton';
+import axios from 'axios';
+
+const VOTE_ITEMS = [
+  { team: '스니프', name: '오대균', id: 17 },
+  { team: '스니프', name: '송지석', id: 15 },
+  { team: '로컬무드', name: '김지원', id: 11 },
+  { team: '로컬무드', name: '김현민', id: 12 },
+  { team: '레디', name: '노이진', id: 13 },
+  { team: '레디', name: '신동현', id: 16 },
+  { team: '갓챠', name: '변지혜', id: 14 },
+  { team: '갓챠', name: '이은비', id: 19 },
+  { team: '셰어마인드', name: '이규호', id: 18 },
+  { team: '셰어마인드', name: '정인영', id: 20 },
+];
+
+const Page = () => {
+  const [selected, setSelected] = useState('');
+
+  const handleVote = async () => {
+    const selectedVoteItem = VOTE_ITEMS.find((item) => item.name === selected);
+
+    if (!selectedVoteItem) {
+      console.error('No selection made');
+      return;
+    }
+
+    try {
+      const response = await axios.post('/api/partvote', {
+        id: selectedVoteItem.id,
+      });
+
+      console.log('투표 성공:', response.data);
+    } catch (error) {
+      console.error('투표 실패:', error);
+    }
+  };
+
+  return (
+    <div className="p-8">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-6">파트장 투표</h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
+          {VOTE_ITEMS.map((item, index) => (
+            <VoteItem
+              key={index}
+              team={item.team}
+              name={item.name}
+              selected={selected === item.name}
+              onChange={() => setSelected(item.name)}
+              isPart={true}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mt-8 flex justify-center">
+        <HoveringButton
+          handleClickButton={handleVote}
+          buttonStyle="w-[100px] h-[60px] mt-4 md:w-[200px]"
+        >
+          투표하기
+        </HoveringButton>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
