22b79691ef3530a4bc5759252a3e77fffda16448
Jiseok <chae0_@naver.com>
2023-12-23 16:16:29 +0900

    style: css 수정

--- components/vote/VoteResult.tsx
+++ components/vote/VoteResult.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
 interface VoteResultItemProps {
-  number: string;
+  number?: string;
   name: string;
   teamname: string;
   votes: string;
 }
 
-const VoteResultItem = ({ number, name, teamname, votes }: VoteResultItemProps) => {
+const VoteResultItem = ({
+  number,
+  name,
+  teamname,
+  votes,
+}: VoteResultItemProps) => {
   return (
-    <div className="bg-white border-2 border-[#3B82F6] text-[#1E40AF] p-4 rounded-lg flex justify-between items-center w-3/4 mx-auto">
-      <div className="font-bold text-lg">
-        <span className='mr-4'>{number}</span>
-        <span className="ml-2">{name}</span>
+    <div className="bg-white border-2 border-[#3B82F6] text-[#1E40AF] p-4 rounded-lg flex items-center w-3/4 mx-auto gap-4">
+      <div className="flex-1 flex items-center">
+        {number && <span className="font-bold text-lg mr-4">{number}</span>}
+        <span className="font-bold text-lg ml-2 flex-1">{name}</span>
       </div>
-      <span className="font-bold text-lg ml-4">{teamname}</span>
-      <span className="font-bold text-lg">{votes}</span>
+      <span className="font-bold text-lg flex-1 text-center">{teamname}</span>
+      <span className="font-bold text-lg flex-1 text-right">{votes}</span>
     </div>
   );
 };
